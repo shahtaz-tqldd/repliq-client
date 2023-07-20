91adf6f37470700d0c086e678949efc62561d809
Shahtaz Rahman <shahtazrahman17@gmail.com>
2023-07-20 16:55:04 +0600

    features section

---
+++ src/pages/Home/Features/Features.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { sectionWide } from '../../../utiles/tailwindClasses'
+
+const Features = () => {
+  const [features, setFeatures] = useState([])
+  fetch('features.json')
+    .then(res => res.json())
+    .then(data => setFeatures(data))
+  return (
+    <section className={sectionWide}>
+      <div className='grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 lg:gap-10 md:gap-8 gap-4'>
+        {features.map(({ id, name, details, img, bg }) => (
+          <div
+            key={id}
+            className={`${bg} p-6 rounded-2xl hover:-translate-y-2 transform transition duration-300 my-12`}
+          >
+            <lord-icon
+              src={img}
+              target="div"
+              trigger="hover"
+              colors="primary:#121331"
+              style={{ width: "60px", height: "60px" }}
+            ></lord-icon>
+            <h2 className='mt-5 text-xl font-bold'>{name}</h2>
+            <p className='mt-4 text-[#555]'>{details}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  )
+}
+
+export default Features
---
+++ src/utiles/tailwindClasses.js
@@ -0,0 +1,3 @@
+import classNames from "classnames";
+
+export const sectionWide = classNames("max-w-7xl mx-auto px-4 py-5")
